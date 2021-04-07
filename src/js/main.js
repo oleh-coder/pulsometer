4b4964bc9b5b20d4b38d0b0f777c57ab0e2348f5
oleh dev <oleh.devel@gmail.com>
2021-04-08 02:00:09 +0300

    Cверстали та за стилизовали слайдер, slick

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow/arrow-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow/arrow-right.png"></button>',
+    });
+});
